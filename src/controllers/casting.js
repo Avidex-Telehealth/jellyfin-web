eeeeb4d2d8dd8bf6a48053daa16d4337629f5935
Sally Ng <sally.ng@telehealthservices.com>
2022-07-01 12:04:44 -0400

    casting to switch to hdmi1 on tizen

--- src/controllers/casting.js
+++ src/controllers/casting.js
@@ -1,7 +1,35 @@
-class Casting {
-    constructor(view, params) {
-
-    }
+export default function (view) {
+    view.addEventListener('viewdestroy', () => {
+        console.log('I was destroyed');
+    });
+    view.addEventListener('viewbeforehide', () => {
+        console.log('I was viewbeforehide');
+    });
+    view.addEventListener('viewshow', () => {
+        console.log('I was shown');
+        tizen.systeminfo.getPropertyValue(
+            'VIDEOSOURCE',
+            (sources) => {
+                const source = sources.connected.find((label) => {
+                    return (label.type === 'HDMI' && label.number === 1);
+                });
+                if (typeof(source) !== 'undefined' && source !== null) {
+                    tizen.tvwindow.setSource(
+                        source,
+                        () => {
+                            tizen.tvwindow.show(() => { }, null, [
+                                '0',
+                                '0',
+                                '1920',
+                                '1080'
+                            ]);
+                        },
+                        (error) => {
+                            console.log(error);
+                        }
+                    );
+                }
+            }
+        );
+    });
 }
-
-export default Casting;
